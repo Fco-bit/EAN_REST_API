2d776b08dd49088b2df958c9ee17cc8b558f340c
Francisco Rodríguez Pérez <56715417+Fco-bit@users.noreply.github.com>
2023-03-24 14:23:51 +0100

    Initial commit
    Project created by Spring initializer with the corresponding initial dependencies to develop the API REST

---
+++ barcode-rest-api/src/main/java/com/mercadona/barcoderestapi/BarcodeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.barcoderestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcodeRestApiApplication.class, args);
+	}
+
+}
---
+++ barcode-rest-api/src/test/java/com/mercadona/barcoderestapi/BarcodeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.barcoderestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcodeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
